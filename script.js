6faf567e41db254b78ef32536b2606972a5ad380
pierpaolo01 <pierpaolo.pascarella@gmail.com>
2021-07-29 19:13:49 +0200

    2cnd commit

---
+++ script.js
@@ -0,0 +1,76 @@
+
+
+let enterCheckbox = document.querySelector('input[name="enter-todo"]');
+enterCheckbox.addEventListener('click', getNewItem);
+
+let list = document.querySelector('li');
+
+
+
+
+let todoArray = [
+
+];
+
+
+function getNewItem(){
+    if(enterCheckbox.checked === true){ //&& document.queryselector('enter-todo').textContent checks if it's empty
+
+        todoArray.push(
+            {value: document.querySelector('#input-todo').value, 
+            state: true,
+            id : ''}
+        );
+
+        let list = document.querySelector('ul');
+
+        
+           appendListItems();
+
+
+        
+
+        console.log(todoArray);
+    }else if(enterCheckbox.checked === false){
+        document.querySelector('#input-todo').value = '';
+    }
+}
+
+function appendListItems(){
+    let listContainer = document.createElement('div');
+    
+
+    if(todoArray.length > 1){
+
+        list.removeChild(listContainer);
+
+    }
+
+    for(let i=0; i < todoArray.length; i++){
+
+        todoArray[i].id = i;
+        
+        liContainer = document.createElement('div');
+        liContainer.setAttribute('class', 'listItem');
+        let completionCheckbox = document.createElement('input');
+        completionCheckbox.setAttribute('type', 'checkbox');
+        completionCheckbox.setAttribute('name', i);
+        let todoContent = document.createElement('span');
+        todoContent.textContent = todoArray[i].value;
+        let deleteSpan = document.createElement('span');
+        deleteSpan.setAttribute('id', i);
+        deleteSpan.setAttribute('class', 'close');
+        deleteSpan.textContent = "X";
+
+
+        liContainer.appendChild(completionCheckbox);
+
+        liContainer.appendChild(todoContent);
+
+        liContainer.appendChild(deleteSpan);
+
+        listContainer.appendChild(liContainer);
+
+    }
+
+}
