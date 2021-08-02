52f827e47c00d8e7a0983fcf8ad9ec0d34ce7d33
pierpaolo01 <pierpaolo.pascarella@gmail.com>
2021-08-02 13:36:20 +0200

    Main logic completed

--- script.js
+++ script.js
@@ -8,11 +8,10 @@ let unorderedList = document.querySelector('.listView');
 
 
 function getNewItem(){
-    //checks if the checkbox OR enter key is press AND that there's content within
     if(enterCheckbox.checked === true){
         
         appendListItem(document.querySelector('#input-todo').value);
-
+        updateCounter();
     }else if(enterCheckbox.checked === false){//once checkbox is unmarked, resets. 
         document.querySelector('#input-todo').value = '';
     }
@@ -53,18 +52,72 @@ function completeTodoItem(item){
     
     let completionCheckbox = document.querySelector('input[name='+item.name+']');
     let parentElem = completionCheckbox.parentElement;
-    console.log(parentElem);
     if (completionCheckbox.checked === true){
         parentElem.querySelector('span').style.textDecoration = 'line-through';
+        updateCounter();
         
     }else{
         parentElem.querySelector('span').style.textDecoration = 'none';
+        updateCounter();
     }
+
+
 }
 
 function deleteTodoItem(item){
 
     unorderedList.querySelector('#li' +item.id).remove();
+    updateCounter();
 
 }
 
+function updateCounter(){
+
+    let totalLi = unorderedList.childElementCount; 
+
+    let checked = 0;
+
+    for(let i=0; i < totalLi; i++){
+
+        let completionCheckbox = unorderedList.childNodes[i].querySelector('input[type="checkbox"]');
+                if(completionCheckbox){
+                    if(completionCheckbox.checked === true){
+                        checked = 1 + checked;
+                    }
+                }
+            }
+
+    let itemCount = document.querySelector('#itemCount');
+    itemCount.textContent = (totalLi-checked) + " Items remaining ";
+
+}
+
+function clearChecked(){
+    let ulLength = unorderedList.childElementCount;
+    let delItems = [];
+
+    for(let i=0; i < ulLength; i++){
+    
+            let completionCheckbox = unorderedList.childNodes[i].querySelector('input[type="checkbox"]');
+            if(completionCheckbox){
+                if(completionCheckbox.checked === true){
+                    
+                    delItems.push(unorderedList.childNodes[i].querySelector('input[type="checkbox"]'));
+                }
+            }
+        }
+
+        for(let i=0; i < delItems.length; i++){
+            liRemove = delItems[i].parentElement;
+            liRemove.parentElement.remove();
+        }
+
+
+
+    
+}
+
+function showCompleted(){
+    
+
+}
